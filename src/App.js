824545fda72d293303344f8ca419cdd31d1843da
Princess Washington <112123284+princesswG@users.noreply.github.com>
2022-12-09 12:02:53 +0000

    First Changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import { HomePage } from "./components/Homepage";
+
+import { Navbar } from "./components/Navbar.js";
+
+function App() {
+  return (  
+    <> 
+ <Navbar/>
+ <HomePage/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function HomePage(){
+    return(
+        <>
+        <div className="homepage">
+         <h2 className="text-dark
+              mb-6
+              Sentencecase
+              ml-10
+            
+              items-center">Stays in Finland</h2>
+      
+        </div>
+        </>
+    )
+}
